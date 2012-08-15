d0d314837806b1f861a9315f285a4c5aaa953a0e
Alex Vedmidskiy <loafshock@gmail.com>
2012-08-15 20:38:29 +0300

    using listview and simpleadapter for advanced timeline view

--- src/com/bepec/yamba/TimelineActivity.java
+++ src/com/bepec/yamba/TimelineActivity.java
@@ -5,20 +5,30 @@ import android.os.Bundle;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.util.Log;
 
 public class TimelineActivity extends Activity {
+
+	static final String[] FROM= {
+		DbHelper.C_CREATED_AT, DbHelper.C_USER, DbHelper.C_TEXT };
+
+	static final int[] TO = {
+		R.id.textCreatedAt, R.id.textUser, R.id.textText };
+
 	DbHelper dbHelper;
 	SQLiteDatabase db;
 	Cursor cursor;
-	TextView textTimeline;
+	ListView listTimeline;
+	SimpleCursorAdapter cursorAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timeline);
 
-		textTimeline = (TextView) findViewById(R.id.textTimeline);
+		listTimeline = (ListView) findViewById(R.id.listTimeline);
 
 		dbHelper = new DbHelper(this);
 		db = dbHelper.getReadableDatabase();
@@ -38,12 +48,8 @@ public class TimelineActivity extends Activity {
 			DbHelper.C_CREATED_AT + " desc");
 		startManagingCursor(cursor);
 
-		String user, text, output;
-		while (cursor.moveToNext()) {
-			user = cursor.getString(cursor.getColumnIndex(DbHelper.C_USER));
-			text = cursor.getString(cursor.getColumnIndex(DbHelper.C_TEXT));
-			textTimeline.append(String.format("%s: %s\n", user, text));
-		}
+		cursorAdapter = new SimpleCursorAdapter(this, R.layout.tweet, cursor, FROM, TO);
+		listTimeline.setAdapter(cursorAdapter);
 	}
 	
 }
